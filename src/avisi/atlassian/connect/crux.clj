ba82f09dda945955f2e0c738ba346e004e50042e
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2020-04-08 09:22:46 +0200

    Fix lifecycle handler, update Crux
    Update Crux to the newest version with better Kafka support.
    Fixed a bug which occurs when uninstalling addons. Atlassian will send 2
    lifecycle calls:
    
    1. Disabled
    2. Uninstalled
    
    But it does so without waiting for the previous one to return so we need
    to retry when we save because we are almost always looking at a failing
    CAS operation.
    
    Resolves: ATOP-60

--- src/avisi/atlassian/connect/crux.clj
+++ src/avisi/atlassian/connect/crux.clj
@@ -18,27 +18,41 @@
    {}
    m))
 
+(defn update-host! [{:keys [prev-host
+                            new-host
+                            crux-node]}]
+  (loop [tries 0
+         host prev-host]
+    (when (= tries 4)
+      (throw (ex-info "Failed handling lifecycle payload" {:amount-of-times-tried tries})))
+    (let [id (:crux.db/id new-host)
+          tx (crux/submit-tx
+               crux-node
+               [[:crux.tx/cas
+                 host
+                 (merge
+                   host
+                   (assoc new-host
+                          :crux.db/id id))]])]
+      (crux/await-tx crux-node tx (Duration/ofSeconds 10))
+      (when-not (crux/tx-committed? crux-node tx)
+        (recur (inc tries) (crux/entity (crux/db crux-node) id))))))
+
 (defn handle-lifecycle-payload! [crux-node request]
   (let [lifecycle-payload (namespace-all-keys (:body-params request))
         _ (log/info (select-keys lifecycle-payload [:atlassian-connect.host/base-url
                                                     :atlassian-connect.host/client-key
-                                                    :atlassian-connect.host/event-type]) "Installing host")
+                                                    :atlassian-connect.host/event-type]) "Handling host lifecycle call")
         id (client-key->crux-id (:atlassian-connect.host/client-key lifecycle-payload))
         db (crux/db crux-node)
-        prev-lifecycle (crux/entity db id)
-        ;; Check JWT token on subsequent calls, this prevents tinkering with shared-secrets of hosts.
-        _ (when prev-lifecycle
-            (jwt/validate-jwt-token request {:validate-qsh? true
-                                             :shared-secret (:atlassian-connect.host/shared-secret prev-lifecycle)}))
-        tx-result (crux/submit-tx
-                   crux-node
-                   [[:crux.tx/cas
-                     prev-lifecycle
-                     (merge
-                      prev-lifecycle
-                      (assoc lifecycle-payload
-                             :crux.db/id id))]])]
-    (crux/sync crux-node (:crux.tx/tx-time tx-result) (Duration/ofSeconds 10))))
+        prev-lifecycle (crux/entity db id)]
+    ;; Check JWT token on subsequent calls, this prevents tinkering with shared-secrets of hosts.
+    (when prev-lifecycle
+      (jwt/validate-jwt-token request {:validate-qsh? true
+                                       :shared-secret (:atlassian-connect.host/shared-secret prev-lifecycle)}))
+    (update-host! {:prev-host prev-lifecycle
+                   :new-host (assoc lifecycle-payload :crux.db/id id)
+                   :crux-node crux-node})))
 
 (defn get-installation [crux-db client-key]
   (crux/entity crux-db (client-key->crux-id client-key)))
--- test/avisi/atlassian/connect/crux_test.clj
+++ test/avisi/atlassian/connect/crux_test.clj
@@ -2,36 +2,53 @@
   (:require [clojure.test :as t]
             [crux.api :as crux]
             [avisi.atlassian.connect.crux :as connect-crux]
-            [crux.io :as cio])
-  (:import [java.io Closeable]))
+            [crux.io :as cio]
+            [cambium.core :as log]))
 
 (def ^:dynamic *crux-node* nil)
 
-(def test-opts {:crux.node/topology :crux.standalone/topology
-                :crux.node/kv-store "crux.kv.memdb/kv"})
+(def test-opts {:crux.node/topology '[crux.standalone/topology crux.kv.rocksdb/kv-store]
+                :crux.kv/sync? false})
 
 (defn with-crux [f]
-  (let [event-log-dir (str (cio/create-tmpdir "event-log-dir"))
-        crux-node (crux/start-node (assoc
-                                    test-opts
-                                    :crux.standalone/event-log-dir event-log-dir))]
-    (binding [*crux-node* crux-node]
-      (try
-        (f)
-        (finally
-          (cio/delete-dir event-log-dir)
-          (.close ^Closeable *crux-node*))))))
+  (let [event-log-dir (str (cio/create-tmpdir "event-log-dir"))]
+    (try
+      (with-open [crux-node (crux/start-node (assoc
+                                               test-opts
+                                               :crux.kv/db-dir event-log-dir))]
+        ;; This seems to make sure that we don't start our function too fast
+        (crux/sync crux-node)
+        (binding [*crux-node* crux-node]
+          (f)))
+      (finally
+        (cio/delete-dir event-log-dir)))))
 
 (t/use-fixtures :each with-crux)
 
 (t/deftest should-transform-all-host-keys
   (t/is
-   (= {:atlassian-connect.host/base-url "https://avisi-support.atlassian.net"
-       :atlassian-connect.host/client-key "client-key"}
+    (= {:atlassian-connect.host/base-url "https://avisi-support.atlassian.net"
+        :atlassian-connect.host/client-key "client-key"}
       (connect-crux/namespace-all-keys {:baseUrl "https://avisi-support.atlassian.net"
                                         :clientKey "client-key"}))
-   "Should transform all camelCase to namespaced kebab-keys"))
+    "Should transform all camelCase to namespaced kebab-keys"))
 
-(t/deftest ^:kaocha/pending should-save-host
-  (t/is (= {}
-           (crux/status *crux-node*))))
+(t/deftest should-retry-save-host-on-failed-cas-op
+  (t/is (nil? (connect-crux/update-host! {:prev-host nil
+                                          :new-host {:atlassian-connect.host/client-key "test"
+                                                     :atlassian-connect.host/event-type "installed"
+                                                     :crux.db/id (connect-crux/client-key->crux-id "test")}
+                                          :crux-node *crux-node*})))
+  (t/is (nil? (connect-crux/update-host! {:prev-host {:atlassian-connect.host/client-key "test"
+                                                      ;; This is the wrong event-type because we are currently "installed"
+                                                      ;; This will trigger a retry
+                                                      :atlassian-connect.host/event-type "uninstalled"
+                                                      :crux.db/id (connect-crux/client-key->crux-id "test")}
+                                          :new-host {:atlassian-connect.host/client-key "test"
+                                                     :atlassian-connect.host/event-type "enabled"
+                                                     :crux.db/id (connect-crux/client-key->crux-id "test")}
+                                          :crux-node *crux-node*})))
+  (t/is (= {:atlassian-connect.host/client-key "test"
+            :atlassian-connect.host/event-type "enabled"
+            :crux.db/id (connect-crux/client-key->crux-id "test")}
+          (connect-crux/get-installation (crux/db *crux-node*) "test"))))
