b510ececdfee8f20f6af6c2832019ac6a434f36a
Fatih Demir <f.demir@avisi.nl>
2020-12-15 12:18:26 +0100

    Add function to get ngrok for standalone fulcro inspect

--- src/avisi/atlassian/connect/ngrok.clj
+++ src/avisi/atlassian/connect/ngrok.clj
@@ -1,12 +1,13 @@
 (ns avisi.atlassian.connect.ngrok
   (:require
-    [clj-http.client :as http]))
+    [clj-http.client :as http]
+    [clojure.string :as string]))
 
-(defn get-base-url []
+(defn get-base-url* [url]
   (try
     (let [tunnels (->
                     (http/get
-                      "http://localhost:4040/api/tunnels"
+                      url
                       {:accept :json
                        :as :json})
                     (get-in [:body :tunnels]))
@@ -18,3 +19,8 @@
       base-url)
     (catch Exception e
       (throw (ex-info "Failed determining base-url are you sure ngrok is running?" {:error-code :ngrok-down})))))
+
+(defn get-base-url [] (get-base-url* "http://localhost:4040/api/tunnels"))
+
+(defn get-fulcro-inspect-base-url []
+  (string/replace (get-base-url* "http://localhost:4041/api/tunnels") #"https://" ""))
